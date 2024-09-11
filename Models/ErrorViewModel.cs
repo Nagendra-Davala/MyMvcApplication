c22eec39706641d5b1d6282729efd580ec15f7c9
Nagendra Babu DAVALA <nagendra.babu.davala@hexagon.com>
2024-09-11 12:22:03 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyMvcApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
